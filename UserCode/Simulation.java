4d10473b8eba738a664c716313883a1da675cac8
KristopherJR <k.randle@lancaster.ac.uk>
2021-02-19 17:40:55 +0000

    This version saw major changes to the way mouse input was handled. The system now implements the Observer design pattern, meaning mouse input is no longer polled. An event is raised when a user clicks the mouse, and the Simulation class subscribes to that event. The program then does a series of checks to ensure a FishFood can be spawned and that the maximum amount are not already in the aquarium (5). Various aspects of the program also now use the factory to be created, such as the world and the inputhandler. IDisplayable has been fully implemented in this version to the UpdatableTokens. Next version will see the development of Fish spawning bubbles properly.

--- UserCode/Simulation.java
+++ UserCode/Simulation.java
@@ -18,34 +18,38 @@ import java.util.List;
 /**
  * Simulation is the top-level class for the Aquarium simulation.
  * 
- * @author Kristopher Randle and Marc Price
- * @version 19-02-2021, 0.9
+ * @author Kristopher Randle & Marc Price
+ * @version 19-02-2021, 1.0
  */
-public class Simulation
+public class Simulation implements IInputListener
 {
     // instance variables:
     // DECLARE a reference to the IWorld, call it '_world':
     private IWorld _world;
     // DECLARE a reference to the IInput, call it '_input':
-    private IInput _input;  
+    private IInput _input;
+    // DECLARE a reference to an IInputPublisher, call it '_inputPublisher':
+    private IInputPublisher _inputPublisher;
+    // DECLARE an int array, call it '_mPLocation'. Initialise it to {-1,-1}:
+    private int[] _mPLocation = {-1,-1};
     // DECLARE a boolean that signals when the simulation loop should be exited:
     private boolean endSim = false;
+    // DECLARE a boolean that signals when a new 'FishFood' needs to be created, call it spawnFood(), set it to false:
+    private boolean _spawnFood = false;
     // DECLARE a List to store object of type 'IUpdatable', call it '_entities':
     private List<IUpdatable> _entities;
-    
     // DECLARE an ITokenFactory, call it '_updatableFactory':
     private IUpdatableFactory _updatableFactory;
-    
-    // DECLARE an int called jNumber, used for specifying the number of JavaFish to be added to the scene:
-    private int jNumber;
-    // DECLARE an int called pNumber, used for specifying the number of Piranha to be added to the scene:
-    private int pNumber;
-    // DECLARE an int called uNumber, used for specifying the number of Urchins to be added to the scene:
-    private int uNumber;
-    // DECLARE an int called sNumber, used for specifying the number of SeaHorse to be added to the scene:
-    private int sNumber;
-    // DECLARE an int called foodCount, used for limiting the amount of FishFood in the aquarium at any given time to 5:
-    private int foodCount;
+    // DECLARE an int called _jNumber, used for specifying the number of JavaFish to be added to the scene:
+    private int _jNumber;
+    // DECLARE an int called _pNumber, used for specifying the number of Piranha to be added to the scene:
+    private int _pNumber;
+    // DECLARE an int called _uNumber, used for specifying the number of Urchins to be added to the scene:
+    private int _uNumber;
+    // DECLARE an int called _sNumber, used for specifying the number of SeaHorse to be added to the scene:
+    private int _sNumber;
+    // DECLARE an int called _foodCount, used for limiting the amount of FishFood in the aquarium at any given time to 5:
+    private int _foodCount;
     public static void main(String args[])
     {
         Simulation sim = new Simulation();
@@ -59,23 +63,38 @@ public class Simulation
     public Simulation()
     {
         // INITIALISE instance variables:
-        // _world:
-        _world = new Core();
-        
-        // _input:
-        _input = (IInput) _world;
-        
         // _entities:
         _entities = new ArrayList<IUpdatable>();
-        
         // _updatableFactory:
         _updatableFactory = new UpdatableFactory();
-        
-        jNumber = 10; // INITIALISE jNumber, this specifies that 10 'JavaFish' objects will be added to the aquarium.
-        pNumber = 3; // INITIALISE pNumber, this specifies that 3 'Piranha' objects will be added to the aquarium.
-        uNumber = 3; // INITIALISE uNumber, this specifies that 3 'Urchin' objects will be added to the aquarium.
-        sNumber = 3; // INITIALISE uNumber, this specifies that 3 'SeaHorse' objects will be added to the aquarium.
-        foodCount = 1; // INITIALISE foodCount.
+        try
+        {
+            // INITIALISE '_world' by returning a 'Core' class object from the '_updatableFactory'. Cast it to 'IWorld':
+            _world = ((IWorld) _updatableFactory.create(Core.class));
+            // _input:
+            _input = (IInput) _world;
+            
+            // INITIALISE '_inputPublisher', create it using the '_updatableFactory' to return a 'MouseHandler' object. Cast it to 'IInputPublisher':
+            _inputPublisher = ((IInputPublisher) _updatableFactory.create(MouseHandler.class));
+            _inputPublisher.Initialise(_input);
+        }
+        catch(Exception e)
+        {
+            // PRINT the error message:
+            System.out.println(e.getMessage());
+        }
+        // Subscribe this instance of 'Simulation' to events published by the '_inputPublisher':
+        _inputPublisher.subscribe(this);
+        // INITIALISE _jNumber, this specifies that 10 'JavaFish' objects will be added to the aquarium:
+        _jNumber = 10; 
+        // INITIALISE _pNumber, this specifies that 3 'Piranha' objects will be added to the aquarium:
+        _pNumber = 3;
+        // INITIALISE _uNumber, this specifies that 3 'Urchin' objects will be added to the aquarium:
+        _uNumber = 3; 
+        // INITIALISE _sNumber, this specifies that 3 'SeaHorse' objects will be added to the aquarium:
+        _sNumber = 3;
+        // INITIALISE _foodCount:
+        _foodCount = 1; 
     }
     
     /**
@@ -87,34 +106,60 @@ public class Simulation
     {
         try
         {
-            //CREATE a specific number of JavaFish in a for loop using jNumber. Add them to the _javaFish List, Then add the bubbles to the _entities List:
-            for(int i = 0; i < jNumber; i++)
+            //CREATE a specific number of JavaFish in a for loop using jNumber. Add them to the _entities List:
+            for(int i = 0; i < _jNumber; i++)
             {
-                // RETURN the new instance of IUpdatable by calling the 'create()' method in the '_updatableFactory':
+                // RETURN the new instance of 'IUpdatable' by calling the 'create()' method in the '_updatableFactory' and store it in '_entities':
                 _entities.add(_updatableFactory.create(UserCode.UpdatableTokens.JavaFish.class));
             }
             //CREATE a specific number of Piranha in a for loop using sNumber. Add them to the _entities List.
-            for(int i = 0; i < pNumber; i++)
+            for(int i = 0; i < _pNumber; i++)
             {
-                // RETURN the new instance of IUpdatable by calling the 'create()' method in the '_updatableFactory':
+                // RETURN the new instance of 'IUpdatable' by calling the 'create()' method in the '_updatableFactory' and store it in '_entities':
                 _entities.add(_updatableFactory.create(UserCode.UpdatableTokens.Piranha.class));
             }
             //CREATE a specific number of Urchins in a for loop using uNumber. Add them to the _entities List.
-            for(int i = 0; i < uNumber; i++)
+            for(int i = 0; i < _uNumber; i++)
             {
-                // RETURN the new instance of IUpdatable by calling the 'create()' method in the '_updatableFactory':
+                // RETURN the new instance of 'IUpdatable' by calling the 'create()' method in the '_updatableFactory' and store it in '_entities':
                 _entities.add(_updatableFactory.create(UserCode.UpdatableTokens.Urchin.class));
             }
             //CREATE a specific number of SeaHorses in a for loop using sNumber. Add them to the _entities List.
-            for(int i = 0; i < sNumber; i++)
+            for(int i = 0; i < _sNumber; i++)
             {
-                // RETURN the new instance of IUpdatable by calling the 'create()' method in the '_updatableFactory':
+                // RETURN the new instance of 'IUpdatable' by calling the 'create()' method in the '_updatableFactory' and store it in '_entities':
                 _entities.add(_updatableFactory.create(UserCode.UpdatableTokens.SeaHorse.class));
             }
         }
         catch(Exception e)
         {
-            //LOL
+            // PRINT the error message:
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    /**
+     * METHOD: This method will iterate through the '_entities' List and inject all of the objects in the List to the 3D world provided as a parameter.
+     * 
+     * An exception is thrown is a user tries to execute this code before the world environment has been created.
+     * 
+     * @author Kristopher Randle
+     * @return void
+     */
+    public void displayEntities()
+    {
+        for(int i = 0; i < _entities.size(); i++)
+        {
+            try
+            {
+                // CALL 'injectDisplayable()' on all objects in the '_entities' List. Cast them to their interface 'IDisplayable' and pass in a reference to the virtual world:
+                ((IDisplayable)(_entities.get(i))).injectDisplayable(_world);
+            }
+            catch(Exception e)
+            {
+                // PRINT the error message:
+                System.out.println(e.getMessage());
+            }
         }
     }
     
@@ -124,50 +169,62 @@ public class Simulation
      * @author Kristopher Randle
      * @return void
      */
-    public void CreateFood() throws WorldDoesNotExistException
+    public void createFood() throws WorldDoesNotExistException
     {
-        // GET the mouse button that was clicked:
-        int _mouseButtonClicked = ((IInput)_world).getMouseClicked();
-        // CHECK if the mouse button clicked was the left mouse button:
-        if(_mouseButtonClicked == 0)
+        // CREATE a new 'FishFood' if an event is raised 
+        if(_spawnFood)
         {
-            // IF there are less than 5 FishFood objects in the aquarium:
-            if(foodCount <= 5)
+            // AND there are less than 5 FishFood objects in the aquarium:
+            if(_foodCount <= 5)
             {
-                // GET the location of the mouse pointer:
-                int[] _mPLocation = ((IInput)_world).getMousePointer();
-                // CREATE a double[] to store the _mPLocation in a format that matches the aquariums co-ordinate system:
-                double[]_mPLD = new double[2];
-                // ALTER the x by factor 0.0077 and y by factor 0.0080 due to discrepancies in the co-ordinates system:
-                _mPLD[0] = _mPLocation[0] * 0.0077;
-                _mPLD[1] = _mPLocation[1] * 0.0080;
-                // CREATE a new FishFood() instance using the factory, store it as an IUpdatable:
+                // RESET '_spawnFood' to false:
+                _spawnFood = false;
+                // CALCULATE the location for the 'FishFood' based on the mouse location, store it in a Double array called '_foodLoc':
+                Double[] _foodLoc = {_mPLocation[0] * 0.0077, _mPLocation[1] * 0.0080, 1.1};
                 try
                 {
-                    IUpdatable food = (_updatableFactory.create(UserCode.UpdatableTokens.FishFood.class));
-                    // CAST  'food' to 'FishFood' and call its 'setDisplayObjectPosition()' method. Pass in the mouse X and Y:
-                    ((FishFood)food).setDisplayObjectPosition(_mPLD[0], _mPLD[1], 1.1);
-                    
-                    // ADD the FishFood to the '_entities' List:
-                    _entities.add(food);
-                    // CALL 'injectDisplayable()' on 'food' and pass in a reference to the virtual world:
-                    ((IDisplayable)food).injectDisplayable(_world);
-                
+                    // CREATE a new 'FishFood' using the '_updatableFactory', call it '_newFood':
+                    IUpdatable _newFood = _updatableFactory.create(FishFood.class);
+                    // SET the location of '_newFood' using '_foodLoc', cast to 'IDisplayObject' to use the 'position()' method:
+                    ((FishFood)_newFood).setDisplayObjectPosition(_foodLoc[0], _foodLoc[1], _foodLoc[2]);
+                    // ADD the '_newFood' to the '_entities' List:
+                    _entities.add(_newFood);
+                    // DISPLAY the '_newFood' in the '_world' by injecting the DisplayObject:
+                    ((IDisplayable)_newFood).injectDisplayable(_world);
                     // INCREMENT the foodCount:
-                    foodCount++;
+                    _foodCount++;
                 }
                 catch(Exception e)
                 {
-                    // LOL
+                    // PRINT the error message:
                     System.out.println(e.getMessage());
                 }
             }
             else
             {
+                // RESET '_spawnFood' to false:
+                _spawnFood = false;
                 // PRINT an error to the user stating they can't make more food:
                 System.out.println("Can't spawn more than 5 FishFood at a time!!!");
             }
         }
+    }
+    
+    // ------------------------------ IMPLEMENTATION OF IInputListener ------------------------------ //
+    /**
+     * Method to handle an input event.
+     * 
+     * Interface author: Marc Price.
+     * Method from: Chris Blythe, week 17 live coding video on BlackBoard.
+     * 
+     * @param data an array of integers containing the input data.
+     */
+    public void onInput(int ...data)
+    {
+        // SIGNAL that a new 'FishFood' needs to be created using the boolean '_spawnFood':
+        _spawnFood = true;
+        // STORE the mouse data provided to '_mPLocation':
+        _mPLocation = data;
     }
 
     /**
@@ -182,14 +239,7 @@ public class Simulation
         try
         {
             // ADD Objects to 3D world?:
-            for(int i = 0; i < _entities.size(); i++)
-            {
-                // CALL 'injectDisplayable()' on all objects in the '_entities' List. Cast them to their interface 'IDisplayable' and 
-                // pass in a reference to the virtual world:
-                ((IDisplayable)(_entities.get(i))).injectDisplayable(_world);
-                
-            }
-            
+            this.displayEntities();
             // Start simulation loop:
             while (!endSim)
             {
@@ -199,26 +249,26 @@ public class Simulation
                 {
                     // SET: render loop exit condition
                     endSim = true;
-                }       
-                // UPDATE Objects in 3D world:
+                }   
+                // UPDATE Objects in '_entities' List:
                 for(int i = 0; i < _entities.size(); i++)
                 {
                     _entities.get(i).update();
                 }
-                
-                CreateFood();
-            
-                // UPDATE 3D World:
-                // Apply all updates to the environment.
-                // This must be called at the end of each pass through the simulation loop.
-                // Cast to IUpdatable interface
+                // UPDATE the '_inputPublisher':
+                ((IUpdatable)_inputPublisher).update();
+                // UPDATE the '_world':
                 ((IUpdatable)_world).update();
+                
+                // Attempt to create a new FishFood:
+                this.createFood();
             }
             // EXIT: cleanly by closing-down the environment:
             _world.destroy();
         }
         catch (WorldDoesNotExistException e)
         {
+            // PRINT the error message:
             System.out.println(e.getMessage());
         }
     }
